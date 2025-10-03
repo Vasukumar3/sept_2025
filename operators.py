d07799bfc77cc829b12b0e301631a76c6918f2df
PALANI VASU KUMAR <128493467+Vasukumar3@users.noreply.github.com>
2025-10-03 19:41:15 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,178 @@
+#operators
+
+#operator ---> symbol or keyword that performs an operation on one or more operands
+#operand --->  Value or variable acted  upon by an operator to produce a result
+
+#addition (+)
+# num_1 = 10
+# num_2 = 10
+# result = num_1 + num_2
+# print(result)
+
+# - (Subtraction): Subtracts the right operand from the left operand.
+# num_1 = 10
+# num_2 = 5
+# result = num_2 - num_1
+# print(result)
+
+
+# * (Multiplication): Multiplies two operands.
+# num_1 = 10
+# num_2 = 5
+# result = num_1 * num_2
+# print(result)
+
+
+
+# ** (Exponentiation): Raises the base (left operand) to the power of the exponent (right operand).
+
+# num_1 = 10
+# num_2 = 5
+# result = num_1 ** num_2
+# print(result)
+# print(f"base value {num_1} and expo {num_2} and result is {num_1 ** num_2}")
+
+#  (Division): Divides the left operand by the right operand.
+# num_1 = 100
+# num_2 = 50
+# result = num_1 / num_2
+# print(result)
+
+
+# // (Floor Division): Returns the quotient of the division, discarding the fractional part.
+# num_1 = 10
+# num_2 = 3
+# result = num_1 // num_2
+# print(result)
+
+
+
+
+# % (Modulus): Returns the remainder of the division of the left operand by the right operand.
+
+
+# num_1 = 10
+# num_2 = 2
+# result = num_1 % num_2
+# print(result)
+
+
+
+
+
+# a = 10
+# b = 3
+
+# addition = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division = a / b
+# remainder = a % b
+# floor_division = a // b
+# exponentiation = a ** b
+
+# print(addition, subtraction, multiplication, division, remainder, floor_division, exponentiation)
+
+# rice = 1000
+# rice += 10 # eq --> rice = rice + 10
+# print(rice)
+
+
+# rice = 1000
+# rice -= 500 # eq --> rice = rice - 500
+# print(rice)
+
+
+# rice = 1000
+# rice *= 10 # eq --> rice = rice * 10
+# print(rice)
+
+
+# product_cost = 1000
+# product_cost_2 = 1000
+# # print(product_cost==product_cost_2)
+# # print(product_cost!=product_cost_2)
+# # print(product_cost<product_cost_2)
+# # print(product_cost<=product_cost_2)
+# print(product_cost>product_cost_2)
+# print(product_cost>=product_cost_2)
+
+
+
+
+# user_name = "rakesh"
+# password = 12345
+# print(user_name == "rakesh" and password == 1234)
+# print(user_name == "rakesh" or password == 1234)
+
+# sample = True
+# print(not sample)
+
+# sample = False
+# print(not sample)
+
+# list_1 = [1,2,3,4,"raju"]
+# print(id(list_1))
+# list_2 = [1,2,3,4,"raju"]
+# print(id(list_2))
+# print(list_1 is list_2)
+
+# num_1 = 1000**100
+# print(id(num_1))
+# num_2 = 1000**100
+# print(id(num_2))
+# print(num_1 is num_2)
+
+
+# num_1 = 500**100
+# print(id(num_1))
+# num_2 = 500**100
+# print(id(num_2))
+# print(num_1 is not num_2)
+
+
+# list_1 = ["vasu","shanmukh","rakesh","raju","prasad","shiva"]
+# print("vasu" in list_1)
+# print("vasu kumar" not in list_1)
+
+# fruits = ["apples", "oranges","bananas"]
+# print("mangoes" in fruits)
+
+# sample = "pythonlife"
+# print("p" in sample)
+
+#discount calculator
+# product_cost = int(input("enter the product cost: "))
+# discount = int(input("enter the discount "))
+# result = product_cost * (discount/100)
+# product_cost -= result #eq --> pc = pc - res
+# # print(product_cost)
+# print(f"discount given {discount}% and final discount {result} and total cost after discount given {product_cost}")
+
+
+# print(5 < 3 or 2 == 2)
+
+
+# num_1 = 10
+# num_2 = 3
+# result = num_1 // num_2
+# # print(result)
+# print(f"divisor {num_1} and dividend {num_2} result is quotient {result}  ")
+
+
+
+# num_1 = 10**1000
+# print(id(num_1))
+# num_2 = 10**1000
+# print(id(num_2))
+# print(num_1 is num_2)
+# print(num_1 is not num_2)
+
+
+
+# sample = True
+# print(not sample)
+
+
+
+
